91467728e9c430c4e268bb8ebe67012eb0633931
Anastasia Mikhailova <amikhail@akamai.com>
2021-01-07 09:40:20 +0100

    fix: correct styles for delete modal

--- src/components/DeleteModalContent/DeleteModalContent.jsx
+++ src/components/DeleteModalContent/DeleteModalContent.jsx
@@ -4,9 +4,9 @@ export const DeleteModalContent = ({ children }) => {
   return (
     <>
       {children}
-      <h2 className='delete'>DELETE MOVIE</h2>
-      <p className='message'>Are you sure you want to delete this movie?</p>
-      <button className='confirm'>CONFIRM</button>
+      <h2>DELETE MOVIE</h2>
+      <p className="message">Are you sure you want to delete this movie?</p>
+      <button className="confirm">CONFIRM</button>
     </>
   );
 };
